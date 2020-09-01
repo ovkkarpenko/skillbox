ee1dafe43950eeca2f86d1b96c30eb0e00344e04
Alex <sanya1444@gmail.com>
2020-09-01 09:27:05 +0300

    fix Task2

--- lesson_3/Controlles/Task2ViewController.swift
+++ lesson_3/Controlles/Task2ViewController.swift
@@ -13,28 +13,16 @@ class Task2ViewController: UIViewController {
     
     @IBOutlet weak var queryTextfield: UITextField!
     
-    var timer: Timer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        queryTextfield.reactive.text
+        _ = queryTextfield.reactive.text
+            .debounce(for: 0.5)
             .observeNext(with: { text in
-                if let timer = self.timer {
-                    timer.invalidate()
+                if let text = text {
+                    print("Sending a request for '\(text)'")
                 }
-                
-                self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { _ in
-                    if let text = text {
-                        print("Sending a request for '\(text)'")
-                    }
-                })
-            }).dispose(in: reactive.bag)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        reactive.bag.dispose()
+            })
     }
     
 }
