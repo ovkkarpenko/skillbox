606325aa2d6ee937c3b7747c54c8a23d16209cdd
Alex <sanya1444@gmail.com>
2020-09-16 12:47:29 +0300

    init

---
+++ lesson_9_2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  lesson_9_2
+//
+//  Created by Oleksandr Karpenko on 16.09.2020.
+//  Copyright © 2020 Oleksandr Karpenko. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
