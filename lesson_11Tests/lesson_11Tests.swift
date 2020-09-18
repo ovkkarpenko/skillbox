e9fd5228be98524915ef20e0eb9d65543e2f34b9
Alex <sanya1444@gmail.com>
2020-09-18 18:48:33 +0300

    fix

--- lesson_11Tests/lesson_11Tests.swift
+++ lesson_11Tests/lesson_11Tests.swift
@@ -10,14 +10,6 @@ import XCTest
 
 class lesson_11Tests: XCTestCase {
     
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testValidUser() throws {
         //given
         let user1 = User(email: "qwerty@ru.ry", password: "qwertYYYYY")
