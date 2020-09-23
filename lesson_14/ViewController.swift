3fc0637b5fabfc1a4396d095a50cc14284e66801
Alex <sanya1444@gmail.com>
2020-09-23 10:39:03 +0300

    Initial Commit

---
+++ lesson_14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_14
+//
+//  Created by Oleksandr Karpenko on 23.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
